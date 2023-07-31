2953f6d46d8f65749fb6a4e43745684713f68068
Yash Pandey <yashpp2014@gmail.com>
2023-07-31 15:42:31 +0530

    resume_app.py modified

--- resume_app.py
+++ resume_app.py
@@ -73,6 +73,44 @@
 # if __name__ == "__main__":
 #     main()
 
+# import io
+# import string
+# from resume_parser import resumeparse
+# from pyresparser import ResumeParser
+# import streamlit as st
+
+# def process_resume_matching(resume_path, requirement_path):
+#     data = ResumeParser(resume_path).get_extracted_data()
+#     have_skills = {skill.lower().translate(str.maketrans('', '', string.punctuation)) for skill in data['skills']}
+    
+#     req_skills = resumeparse.read_file(requirement_path)
+#     skills = {skill.lower() for skill in req_skills['skills']}
+    
+#     matched_skills = have_skills & skills
+#     per = (len(matched_skills) / len(skills)) * 100
+    
+#     return per, matched_skills
+
+# def main():
+#     st.title("Skill Matching from Resume")
+#     st.write("Upload your resume and a requirement file to check how many skills you've matched.")
+
+#     # File upload
+#     resume_input = st.file_uploader("Upload your resume (PDF)", type=["pdf"])
+#     requirement_input = st.file_uploader("Upload the requirement file (PDF)", type=["pdf"])
+
+#     if resume_input and requirement_input:
+#         resume_path = io.BytesIO(resume_input.read())
+#         requirement_path = io.BytesIO(requirement_input.read())
+
+#         matching_percentage, matched_skills = process_resume_matching(resume_path, requirement_path)
+
+#         st.write(f"Matching Skills: {', '.join(matched_skills)}")
+#         st.write(f"Matching Percentage: {matching_percentage:.2f}%")
+
+# if __name__ == "__main__":
+#     main()
+
 import io
 import string
 from resume_parser import resumeparse
@@ -82,13 +120,13 @@ import streamlit as st
 def process_resume_matching(resume_path, requirement_path):
     data = ResumeParser(resume_path).get_extracted_data()
     have_skills = {skill.lower().translate(str.maketrans('', '', string.punctuation)) for skill in data['skills']}
-    
+
     req_skills = resumeparse.read_file(requirement_path)
     skills = {skill.lower() for skill in req_skills['skills']}
-    
-    matched_skills = have_skills & skills
+
+    matched_skills = have_skills.intersection(skills)
     per = (len(matched_skills) / len(skills)) * 100
-    
+
     return per, matched_skills
 
 def main():
